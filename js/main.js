380d9dfbd1451c96c10dababfa162bfe2ab72125
unknown <sayed.ahmed.elbadry@gmail.com>
2021-09-16 06:12:06 -0700

    add dark mode

--- js/main.js
+++ js/main.js
@@ -3,18 +3,48 @@ new WOW().init();
 document.querySelectorAll("h1,h2,h3").forEach((h) =>{
     h.className = ('wow animate__bounceInUp')
 })
-
+// Dark Mode
+let dark = document.getElementById('dark-mode');
+dark.addEventListener('click', () => {
+    document.querySelectorAll('section').forEach( (s) => {
+        if(s.classList.contains('dark' )){
+            s.classList.remove('dark');
+            document.documentElement.style.setProperty('--bg-section-color',"#f7f9fe");
+            document.documentElement.style.setProperty('--bg-card',"#fff");
+            dark.classList.remove('fa-moon');
+            dark.classList.add('fa-sun');
+            
+        }else{
+            s.classList.add('dark');
+            document.documentElement.style.setProperty('--bg-section-color',"rgb(15, 12, 12)");
+            document.documentElement.style.setProperty('--bg-card',"#283a5f");
+            dark.classList.remove('fa-sun');
+            dark.classList.add('fa-moon')
+        }
+        
+    })
+    document.querySelectorAll('div,h5,.answer>li,nav li>a').forEach((ele) => {
+        if(ele.classList.contains('dark-color')){
+            ele.classList.remove('dark-color')
+        }else{
+            ele.classList.add('dark-color')
+        }
+    })
+    
+})
 // show icon scroll up
 let iconUp = document.getElementById('icon-up');
 let nav = document.querySelector('nav')
 window.onscroll = function () {
     
-    if(this.scrollY >= 700){
+    if(this.scrollY >= 800){
         iconUp.classList.add('show');
-        nav.classList.add('show')
+        nav.classList.add('show');
+        dark.classList.add('up')
     }else{
         iconUp.classList.remove('show');
         nav.classList.remove('show');
+        dark.classList.remove('up')
     }
 }
 iconUp.onclick = function (){
@@ -23,8 +53,28 @@ iconUp.onclick = function (){
         behavior: "smooth"
     })
 }
+// nav scroll active section
+const sections = document.querySelectorAll('section');
+const navLi = document.querySelectorAll('nav .container ul li');
+// console.log(navLi)
+window.addEventListener('scroll', ()=> {
+    let current = '';
+    sections.forEach( section => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.clientHeight;
+        if(pageYOffset >= sectionTop -sectionHeight / 3){
+            current = section.getAttribute('id')
+        }
+    })
+    navLi.forEach(li => {
+        li.classList.remove('active');
+        if(li.classList.contains(current)){
+            li.classList.add('active')
+        }
+    })
+})
 // creat overlay
-let overlay = document.createElement('div');
+const overlay = document.createElement('div');
 overlay.className = ('overlay');
 document.body.appendChild(overlay)
 // event icon show nav
